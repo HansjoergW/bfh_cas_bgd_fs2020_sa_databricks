d2a7dfbc99a99de36d9756e0568cdae542a3feed
HansjoergW <hansjoerg.wingeier@gmail.com>
2020-08-17 14:42:07 +0200

    polyglot

--- notebooks/Users/hansjoerg.wingeier@mobi.ch/notebooks/GitDemo.py
+++ notebooks/Users/hansjoerg.wingeier@mobi.ch/notebooks/GitDemo.py
@@ -1,1 +1,18 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # some markdown
+
+# COMMAND ----------
+
+# MAGIC %Python
+# MAGIC import os
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC rm /mynewFolder/file.txt
+
+# COMMAND ----------
+
+# MAGIC %SQL
+# MAGIC SELECT * FROM MY_TABLE WHERE AGE > 10;
